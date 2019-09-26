1bc2359a7b87829f13dc33833a05db5184a69dab
BetA <beta.underalpha@gmail.com>
2019-09-26 15:57:03 +0900

    ASSN_1 add comment

--- ASSN_1/assn1_1.c
+++ ASSN_1/assn1_1.c
@@ -1,4 +1,4 @@
-#include <stdio.h> // 표준입출력과 관련된 header를 include
+#include <stdio.h> // 표준입출력 라이브러리 include
 #include <math.h>  // 수학 관련 함수를 이용하기 위해 math.h를 include
 
 int exchange(int target, int *amount); // target 단위의 달러 개수를 반환해주고, 반환한 만큼 amount를 감소시키는 함수
@@ -8,7 +8,7 @@ int main() { // main에 프로그램 구현
 
     int amount = 0; // 입력받는 원 화의 양
     int ex_rate = 0; // 입력받는 환율
-    int MUL = 100; // 소수 둘째자리까지 표현하기 위한 보정 상수
+    int MUL = 100; // int 형을 이용하여 소수 둘째자리까지 표현하기 위한 보정 상수
 
     // 원 화 입력
     printf("Enter the amount of exchange: ");
@@ -34,14 +34,14 @@ int main() { // main에 프로그램 구현
     return 0;
 }
 
-int exchange(int target, int *amount){
+int exchange(int target, int *amount){ // target 단위의 달러 개수를 반환해주고, 반환한 만큼 amount를 감소시키는 함수
     printf("%d $:    %d\n", target, *amount/target); // target 단위의 달러 개수 출력
     *amount = *amount - target * (*amount/target); // 변환한 만큼 amount 차감
 
     return 0;
 }
 
-int exchange_cent(int target, int *amount){
+int exchange_cent(int target, int *amount){ // exchange와 같은 역할을 하되, cent 단위로 printf 하기 위해 선언
     printf("%d cent:    %d\n", target, *amount/target); // target 단위의 cent 개수 출력
     *amount = *amount - target * (*amount/target); // 변환한 만큼 amount 차감
 
--- ASSN_1/assn1_2.c
+++ ASSN_1/assn1_2.c
@@ -1,43 +1,46 @@
-#include <stdio.h>
-#include <math.h>
+#include <stdio.h> // 표준입출력 라이브러리 include
+#include <math.h>  // 수학 관련 함수를 이용하기 위해 math.h를 include
 
-float calc_Vs(float temp);
-float calc_distance(float t, float Vs);
-void get_data(float *time1, float *time2);
+float calc_Vs(float temp); // 섭씨 온도에 따른 음속을 반환
+float calc_distance(float t, float Vs); // 시간과 음속을 입력받아 물체와의 거리를 반환
+void get_data(float *time1, float *time2); // 사용자로부터 time1과 time2를 입력받음
 
 int main(void) {
 
-    float degree = 0;
-    float vs = 0;
+    float degree = 0; // 섭씨 온도를 입력받는 변수
+    float vs = 0; // 음속을 저장하는 변수
 
-    float time1 = 0;
-    float time2 = 0;
+    float time1 = 0; // 1번째 시간 측정을 입력받는 변수
+    float time2 = 0; // 2번째 시간 측정을 입력받는 변수
 
+    // 섭씨 온도 입력
     printf("Input the temperature in Celsius: ");
     scanf("%f", &degree);
 
+    // 음속 계산 및 출력
     vs = calc_Vs(degree);
-    printf("Speed of sound is %.2f m/s\n", vs);
+    printf("Speed of sound is %.2f m/s\n", vs); // 소수 둘째자리까지 출력하기 위해 %.2f 사용
 
+    // 시간 입력받고 거리 출력
     get_data(&time1, &time2);
-    printf("The distance is %.2f m\n", calc_distance((time2 + time1) / 2, vs));
+    printf("The distance is %.2f m\n", calc_distance((time2 + time1) / 2, vs)); // 소수 둘째자리까지 출력하기 위해 %.2f 사용
 
     return 0;
 
 }
 
-float calc_Vs(float temp) { // 음속을 계산
+float calc_Vs(float temp) { // 섭씨 온도에 따른 음속을 반환
     return (331.3 + 0.606 * temp);
 }
 
 
-float calc_distance(float t, float Vs) { // 물체와의 거리를 구하여 반환
+float calc_distance(float t, float Vs) { // 시간과 음속을 입력받아 물체와의 거리를 반환
     return (t * Vs) / 2;
 }
 
-void get_data(float *time1, float *time2) {
+void get_data(float *time1, float *time2) { // 사용자로부터 time1과 time2를 입력받음
     printf("Input the first measured time (seconds): ");
-    scanf("%f", time1);
+    scanf("%f", time1); // time1은 포인터 변수이므로 &time1과 같이 작성하지 않는다.
     printf("Input the second measured time (seconds): ");
-    scanf("%f", time2);
+    scanf("%f", time2); // time2는 포인터 변수이므로 &time2와 같이 작성하지 않는다.
 }
