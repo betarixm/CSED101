be03d891ddf4923c3f8c57313b398ba4095590ef
beta-lux <beta.underalpha@gmail.com>
2019-09-27 10:15:36 +0900

    add Lab2_1

--- Lab/20190084_Lab2_1.c
+++ Lab/20190084_Lab2_1.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 
+int add(int num1, int num2);
+float average(int num1, int num2);
+void printResult(int x, int y, int sum, float avg);
+
 int main(void){
     int num1 = 0;
     int num2 = 0;
+	int sum = 0;
+	float avg = 0;
 
     printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
+    
+	sum = add(num1, num2);
+	avg = average(num1, num2);
+
+	printResult(num1, num2, sum, avg);
+
+	return 0;
+
 }
 
+int add(int num1, int num2){
+	return num1 + num2;
+}
+
+float average(int num1, int num2){
+	return (num1 + num2) / 2
+}
+
+void printResult(int x, int y, int sum, float avg){
+	printf("The sum of %d and %d is %d", x, y, sum);
+	printf("The average of numbers is %.1f", avg);
