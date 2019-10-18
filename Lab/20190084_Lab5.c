135fea34c4b3edbb04524575f751941a7b88fcd3
beta-lux <beta.underalpha@gmail.com>
2019-10-18 10:35:33 +0900

    Lab5

---
+++ Lab/20190084_Lab5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int seqSearch(int arr[], int target, int size);
+
+int main(void){
+	int i = 0;
+	int target = 0;
+	int resultIdx = 0;
+	int input[10] = { 0 };
+
+	printf("Input 10 integer values: ");
+	
+	for (i = 0; i < 10; i++){
+		scanf("%d", &input[i]);
+	}
+	
+	printf("Entered number is        ");
+
+	for (i = 0; i < 10; i++){
+		printf("%d ", input[i]);
+	}
+
+	printf("\n");
+	
+	printf("Input an integer that you want to find: ");
+	scanf("%d", &target);
+
+	resultIdx = seqSearch(input, target, 10);
+
+	if (resultIdx == -1){
+		printf("Not Found\n");
+	} else {
+		printf("The index of %d is %d\n", target, resultIdx);
+	}
+
+	return 0;
+}
+
+int seqSearch(int arr[], int target, int size){
+	int i = 0;
+	for(i = 0; i < size; i++){
+		if(arr[i] == target)
+			return i;
+	}
+
+	return -1;
+}
+	
