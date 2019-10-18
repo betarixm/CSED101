49430a9a91be4b0d86a7099901ade0732256e27c
beta-lux <beta.underalpha@gmail.com>
2019-10-18 10:40:21 +0900

    hotfix: Refactor

--- Lab/20190084_Lab5.c
+++ Lab/20190084_Lab5.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int seqSearch(int arr[], int target, int size);
@@ -5,7 +6,7 @@ int seqSearch(int arr[], int target, int size);
 int main(void){
 	int i = 0;
 	int target = 0;
-	int resultIdx = 0;
+	int idx = 0;
 	int input[10] = { 0 };
 
 	printf("Input 10 integer values: ");
@@ -25,12 +26,12 @@ int main(void){
 	printf("Input an integer that you want to find: ");
 	scanf("%d", &target);
 
-	resultIdx = seqSearch(input, target, 10);
+	idx = seqSearch(input, target, 10);
 
 	if (resultIdx == -1){
 		printf("Not Found\n");
 	} else {
-		printf("The index of %d is %d\n", target, resultIdx);
+		printf("The index of %d is %d\n", target, idx);
 	}
 
 	return 0;
