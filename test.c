35e2dc3d909d2068d6032e46798b3ea98ef16923
beta-lux <beta.underalpha@gmail.com>
2019-11-15 10:10:50 +0900

    ASSN3

--- test.c
+++ test.c
@@ -1,43 +1,44 @@
+// macOS, Clion에서 작성.
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#define SIZE 20
 
-#define ROW 9
-#define COL 9
-#define CORNER 1000
-#define LINE_HORI 2000
-#define LINE_VERT 3000
-#define CELL 4000
-
-#define P1 1
-#define P2 2
-#define P 10
-#define AI 20
-
-#define EASY 100
-#define NORMAL 200
-#define HARD 300
-
-#define MAX_TURN 40
-#define MAX_SIMUL 20
-
-void init_lines(int lines[][2]){
-    int row = 0, col = 0, idx = 0;
-    for(row = 0; row < ROW; row++){
-        for(col = ((row%2) == 0); col < COL; col+=2){
-            lines[idx][0] = row;
-            lines[idx][1] = col;
-            idx++;
-        }
-    }
-}
+void print_matrix(int** arr, int size);
 
 int main(){
-    int lines[MAX_TURN][2] = {0};
-    int i = 0;
-    init_lines(lines);
+    int **arr;
+    int size = 0, row = 0, col = 0;
+    int isUpper = 0;
+    int isLower = 0;
+    printf("Enter the size of square: ");
+    scanf("%d", &size);
 
-    for (i = 0; i < MAX_TURN; i++){
-        printf("%d %d\n", lines[i][0], lines[i][1]);
+    arr = (int **)calloc(size, sizeof(int));
+
+    for(row = 0; row < size; row++){
+        arr[row] = (int*)calloc(size, sizeof(int));
     }
 
-    return 0;
+    for(row = 0; row < size; row++){
+        for(col = 0; col < size; col++){
+            isUpper = (col < (size - (row + 1)));
+            isLower = (col > (size - (row + 1)));
+            arr[row][col] = arr[row][col] + isUpper - isLower;
+        }
+    }
+
+    print_matrix(arr, size);
+}
+
+void print_matrix(int** arr, int size){
+    int row = 0, col = 0;
+
+    for(row = 0; row < size; row++){
+        for(col = 0; col < size; col++){
+            printf("%3d", arr[row][col]);
+        }
+        printf("\n");
+    }
 }
